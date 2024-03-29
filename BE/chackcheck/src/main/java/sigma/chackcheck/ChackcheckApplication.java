35faab9b51447016addde8b03dfe2049af44824d
myeongheon <axm0219@naver.com>
2024-03-29 15:57:02 +0900

    BE 초기세팅

---
+++ BE/chackcheck/src/main/java/sigma/chackcheck/ChackcheckApplication.java
@@ -0,0 +1,13 @@
+package sigma.chackcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChackcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChackcheckApplication.class, args);
+	}
+
+}
