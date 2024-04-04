eb507793242545e151785e43418b40a3841b945e
박민규 <pmq0101@naver.com>
2024-04-05 01:39:13 +0900

    feat: create BookReserve Entity

---
+++ BE/chackcheck/src/main/java/sigma/chackcheck/BookReserve.java
@@ -0,0 +1,15 @@
+package sigma.chackcheck;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class BookReserve {
+    @Id
+    private Long id;
+}
