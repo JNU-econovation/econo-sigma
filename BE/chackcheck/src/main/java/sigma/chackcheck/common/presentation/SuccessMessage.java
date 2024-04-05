da49e09443dd55d9a4db2e2f7f4641fff5a003df
myeongheon <axm0219@naver.com>
2024-04-05 16:43:14 +0900

    refactor: 응답 메세지 수정

--- BE/chackcheck/src/main/java/sigma/chackcheck/common/presentation/SuccessMessage.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/common/presentation/SuccessMessage.java
@@ -2,8 +2,7 @@ package sigma.chackcheck.common.presentation;
 
 public enum SuccessMessage {
     CREATE("생성 성공"),
-    GET_BOOK("도서 조회 성공"),
-    GET_BOOK_DETAIL("도서 상세 조회 성공"),
+    GET("조회 성공"),
     UPDATE("수정 성공"),
     DELETE("삭제 성공");
     private final String message;
