7ee23da04d21e2209868b7fb6c10326b48ffcc2a
박민규 <pmq0101@naver.com>
2024-04-05 22:56:56 +0900

    feat: 연관관계 추가

--- BE/chackcheck/src/main/java/sigma/chackcheck/Penalty.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/Penalty.java
@@ -2,9 +2,13 @@ package sigma.chackcheck;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.*;
 
 import java.time.LocalDate;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter
@@ -17,5 +21,8 @@ public class Penalty {
     // 대출/반납 제한 만료일자
     private LocalDate expiryDate;
 
-
+    // 유저 일대일 ***
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
