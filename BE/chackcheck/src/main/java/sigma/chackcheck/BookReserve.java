fbbc136fab32a14d6a4647851972fffe4321be9c
박민규 <pmq0101@naver.com>
2024-04-05 22:56:42 +0900

    feat: 연관관계 추가

--- BE/chackcheck/src/main/java/sigma/chackcheck/BookReserve.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/BookReserve.java
@@ -1,8 +1,9 @@
 package sigma.chackcheck;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter
@@ -12,4 +13,13 @@ import lombok.*;
 public class BookReserve {
     @Id
     private Long id;
+
+    // 유저 다대일 ***
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    // 도서 대출/반납 일대일 ***
+    @OneToOne
+    @JoinColumn(name = "bookBorrow_id")
+    private BookBorrow bookBorrow;
 }
