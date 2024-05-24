07c78e8be1cba6f018ae38f5b4183c4f6ee76539
suin <suin020226@naver.com>
2024-05-24 12:30:36 +0900

    fix:mainpage

--- FE/src/App.js
+++ FE/src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route, Router, useLocation} from 'react-router-dom';
 import './App.css';
 
 import {React, useState , useEffect} from 'react';
-import { Routes, Route} from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 import Header from './components/common/header/header.jsx';
@@ -11,34 +10,35 @@ import Category from './components/common/Category.jsx';
 import Main from './pages/main';
 import Login from './pages/login';
 import Loading from './components/common/Loading.jsx';
-import Paging from './components/common/pagination.jsx';
+//import Paging from './components/common/pagination.jsx';
 
-import Book from './pages/book.jsx'
+import Book from './pages/book.jsx';
+
 function App() {
   const currentLocation = useLocation();
 
-  const [loading, setLoading] = useState(true);
-  const [book, setBook] = useState([]);
+  //const [loading, setLoading] = useState(true);
+  //const [book, setBook] = useState([]);
 
 
-  const getBook = async () => {
-    try {
-        const response = await fetch('http://localhost:3001/db'); // 서버에서 데이터를 가져옴
-        const json = await response.json(); // 응답을 JSON으로 변환
-        setBook(json); // 상태를 업데이트
-    } catch (error) {
-        console.error('Fetching books failed:', error); // 오류가 발생한 경우 콘솔에 오류 메시지 출력
-    } finally {
-        setLoading(false); // 로딩 상태를 false로 설정
-    }
-};
+  //const getBook = async () => {
+    //try {
+       // const response = await fetch('http://localhost:3001/db'); // 서버에서 데이터를 가져옴
+        //const json = await response.json(); // 응답을 JSON으로 변환
+        //setBook(json); // 상태를 업데이트
+  //  } catch (error) {
+  //      console.error('Fetching books failed:', error); // 오류가 발생한 경우 콘솔에 오류 메시지 출력
+  //  } finally {
+    //    setLoading(false); // 로딩 상태를 false로 설정
+   // }
+//};
 
 
-  useEffect(() => {
-    getBook()
-  }, []);
+  //useEffect(() => {
+  //  getBook()
+  //}, []);
 
-  const params = useParams();
+    //const params = useParams();
 
   return (
     <div className="App">
@@ -51,7 +51,7 @@ function App() {
       <Routes>
         <Route path="/books" element={<Main/>}/>
         <Route path="/users/login" element={<Login/>}/>
-        <Route path="/books/:id" element={<Book/>}></Route>
+        {/*<Route path="/books/:id" element={<Book/>}></Route>*/}
       </Routes>
 
       
--- FE/src/pages/main.jsx
+++ FE/src/pages/main.jsx
@@ -1,11 +1,10 @@
 import SearchBar from "../components/common/home/SearchBar";
-import Category from "../components/common/Category";
-import Paging from "../components/common/paginations/pagination";
+import Paging from "../components/common/pagination";
 const Main = () => {
     return (
         <div>
             <SearchBar/>
-            <Paging/>
+            {/* <Paging/> */}
         </div>
     );
 }
