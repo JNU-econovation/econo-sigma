4ab57f2cad6add62e6ac10eabf30f7c7b06d2291
박민규 <pmq0101@naver.com>
2024-06-06 20:34:01 +0900

    feat: JpaRepository

--- BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/repository/BookCategoryRepository.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/repository/BookCategoryRepository.java
@@ -1,7 +1,9 @@
 package sigma.chackcheck.domain.book.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sigma.chackcheck.domain.book.domain.BookCategory;
 
 @Repository
-public interface BookCategoryRepository {
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 }
