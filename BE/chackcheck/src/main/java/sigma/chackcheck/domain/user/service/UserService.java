37b475ddc78187969faef6c57fc04cf4fe9c24dd
myeongheon <axm0219@naver.com>
2024-06-07 13:44:56 +0900

    feat: 유저 아이디로 유저 찾기

--- BE/chackcheck/src/main/java/sigma/chackcheck/domain/user/service/UserService.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/domain/user/service/UserService.java
@@ -1,7 +1,17 @@
 package sigma.chackcheck.domain.user.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sigma.chackcheck.domain.user.domain.User;
+import sigma.chackcheck.domain.user.repository.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
 }
