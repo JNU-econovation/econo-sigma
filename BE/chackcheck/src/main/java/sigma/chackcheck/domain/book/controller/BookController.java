d644d5b9bff8f815df19ad2377f0ae308d89f796
박민규 <pmq0101@naver.com>
2024-06-25 13:17:38 +0900

    feat,refactor: api uri 수정, getBookByCategoryName api 추가

--- BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/controller/BookController.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/controller/BookController.java
@@ -23,12 +23,28 @@ public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping("/books")
+    @GetMapping("/books/all")
     public ApiResponse<SuccessBody<BookPageResponse>> getMainPage(
         @RequestParam(value = "page", defaultValue = "0") int page) {
 
         Page<Book> bookList = bookService.getBookPage(page);
 
+        return getSuccessBodyApiResponse(page, bookList);
+    }
+
+    @GetMapping("/books/category")
+    public ApiResponse<SuccessBody<BookPageResponse>> getBooksByCategoryName(
+        @RequestParam(value = "categoryName") String categoryName,
+        @RequestParam(value = "page", defaultValue = "0") int page
+    ){
+        Page<Book> bookList = bookService.getBookPageByCategoryName(categoryName, page);
+
+        return getSuccessBodyApiResponse(page, bookList);
+    }
+
+    private ApiResponse<SuccessBody<BookPageResponse>> getSuccessBodyApiResponse(
+        @RequestParam(value = "page", defaultValue = "0") int page,
+        Page<Book> bookList) {
         PageInfo pageInfo =  PageInfo.of(page, bookList.getTotalElements(), bookList.getTotalPages());
 
         List<BookDTO> bookDtoList = bookList.getContent().stream()
@@ -42,4 +58,6 @@ public class BookController {
 
         return ApiResponseGenerator.success(bookPageResponse, HttpStatus.OK, SuccessMessage.CREATE);
     }
+
+
 }
