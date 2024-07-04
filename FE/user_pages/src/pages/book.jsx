aa5f4f01bdc718087baa93edaa1eca564f9593db
jiucchu <jiucchu@gmail.com>
2024-07-04 19:32:31 +0900

    fix: api 수정

--- FE/user_pages/src/pages/book.jsx
+++ FE/user_pages/src/pages/book.jsx
@@ -28,7 +28,7 @@ function Book() {
 
   const getInfo = async () => {
     try {
-      const response = await fetch(`http://43.202.196.181:8080/books/${bookId}`); // 서버에서 데이터를 가져옴
+      const response = await fetch(`http://43.202.196.181:8080/books/${bookId}`, {method: 'GET'}); // 서버에서 데이터를 가져옴
       const json = await response.json(); // 응답을 JSON으로 변환
       setInfo(json); // 상태를 업데이트
     } catch (error) {
