76a32677396f551f29d625f5f51691504bf55198
박민규 <pmq0101@naver.com>
2024-07-09 02:47:22 +0900

    feat: toBook, toBookDetail, toBookWithCategories 메서드 추가

--- BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/domain/BookApprove.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/domain/BookApprove.java
@@ -9,11 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
@@ -33,8 +36,46 @@ public class BookApprove {
     private String imageURL;
     // 도서 관련 정보
     private String information;
+    private String categories;
 
 
     @Column(name = "user_id")
     private Long userId;
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Builder
+    public static class BookWithCategories{
+        private Book book;
+        private String categories;
+    }
+
+    public static BookWithCategories toBookWithCategories(Book book, String categories){
+        return BookWithCategories.builder()
+            .book(book)
+            .categories(categories)
+            .build();
+    }
+
+    public static Book toBook(BookApprove bookApprove){
+        return Book.builder()
+            .title(bookApprove.getTitle())
+            .author(bookApprove.getAuthor())
+            .publishYear(bookApprove.getPublishYear())
+            .publisher(bookApprove.getPublisher())
+            .borrowCount(0)
+            .imageURL(bookApprove.getImageURL())
+            .information(bookApprove.getInformation())
+            .build();
+    }
+
+    public static BookDetail toBookDetail(Book book){
+        return BookDetail.builder()
+            .title(book.getTitle())
+            .borrowStatus(false)
+            .reserveStatus(false)
+            .book(book)
+            .build();
+    }
 }
