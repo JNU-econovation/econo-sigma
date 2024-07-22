18fa1d61d87eb5b64f14f410a320dd87834a3f6a
박민규 <pmq0101@naver.com>
2024-07-22 15:18:57 +0900

    feat: BookDetail soft Delete 구현

--- BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/domain/BookDetail.java
+++ BE/chackcheck/src/main/java/sigma/chackcheck/domain/book/domain/BookDetail.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 
 @Entity
@@ -14,6 +16,8 @@ import org.hibernate.annotations.OnDeleteAction;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE book_detail SET deleted = true WHERE book_detail_id = ?")
+@Where(clause = "deleted = false")
 public class BookDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +25,7 @@ public class BookDetail {
     private String title;
     private boolean borrowStatus;
     private boolean reserveStatus;
+    private boolean deleted;
 
     @ManyToOne(fetch = LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
